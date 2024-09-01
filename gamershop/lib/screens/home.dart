993ec9ad2776a445681ab394f02f127f72b81c53
abbyspeed <wnabihah09@gmail.com>
2024-09-01 12:03:02 +0800

    updated

--- gamershop/lib/screens/home.dart
+++ gamershop/lib/screens/home.dart
@@ -4,9 +4,16 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Categories')
-      ),
+      body: Column(
+        children: [
+          Text("Welcome"),
+          Text("We sell the best products for you"),
+          ElevatedButton(
+            onPressed: (){}, 
+            child: Text("Shop Now")
+          )
+        ],
+      )
     );
   }
 }
